55f430e138fa6eabff6f62583d07f282be3354b7
Complex-Fisherman-77 <complex_fisherman_77@outlook.com>
2025-04-03 13:10:23 -0300

    Add Google form

--- src/main.tsx
+++ src/main.tsx
@@ -5,10 +5,12 @@ import './index.css'
 import App from './App.tsx'
 import PerpetualWithdrawalRetirementSimulator from './apps/perpetual-withdrawal-retirement-simulator/perpetual-withdrawal-retirement-simulator.tsx'
 import FixedTimeWithdrawalRetirementSimulator from './apps/fixed-time-withdrawal-retirement-simulator/fixed-time-withdrawal-retirement-simulator.tsx'
+import GoogleFormsFeedback from './components/google-forms-feedback/google-forms-feedback.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter basename="/online-finance-calculator">
+      <GoogleFormsFeedback/>
       <Routes>
         <Route path="/" element={<App />} />
         <Route path="/perpetual-withdrawal" element={<PerpetualWithdrawalRetirementSimulator />} />
